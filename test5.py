31c10da7b0765dc5f95086b82048bac34abf0ee9
porchelvan07 <porchelvan07@gmail.com>
2020-05-14 18:37:25 +0530

    initial Work

---
+++ test5.py
@@ -0,0 +1,17 @@
+import os
+path="/home/porchy/Downloads/anydesk-5.5.1/"
+FList=[]
+def init():  
+    for File in os.listdir(path):
+        if File.find('.run')>0 and os.path.isfile(path+File):
+            FList.append(File)
+            #FList.sort()
+    print(FList[1]+" "+FList[3] )
+    
+    if FList[1]>FList[3]:
+        print ("Greater")
+    else:
+        print ("Smaller")
+        
+    print (os.getcwd())
+init() 
